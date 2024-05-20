c9e769735add30fdf3b86670e5f35c9bc4989fa6
Peyman Baghershahi <peyman.baghershahi@gmail.com>
2024-05-20 10:38:53 -0500

    ray added

--- nautilus/installation.sh
+++ nautilus/installation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update;
-apt-get install git screen vim curl wget;
+apt-get install git screen vim curl wget -y;
 
 cd /workspace;
 python3 -m venv env;
@@ -13,6 +13,12 @@ pytorch_version="torch-${pytorch_version}.html";
 pip install --no-index pyg_lib torch-scatter torch-sparse torch-cluster torch-spline-conv -f https://pytorch-geometric.com/whl/$pytorch_version;
 pip install torch-geometric;
 pip install torcheval;
+pip install torchmetrics;
+pip install matplotlib;
+pip install pandas;
 pip install ipdb;
 pip install gdown;
+pip install notebook;
+pip install pyyaml;
+pip install -U "ray[data,train,tune,serve]";
 printf "\033c";
